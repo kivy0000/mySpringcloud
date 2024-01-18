c8c7292f0b9ed752b8cf25121159065fee2855f5
kivy0000 <kivy0000@126.com>
2024-01-18 11:39:15 +0800

    0.2.2

--- member-service-consumer-80/src/main/java/com/kfhstu/ConsumerApplication.java
+++ member-service-consumer-80/src/main/java/com/kfhstu/ConsumerApplication.java
@@ -3,10 +3,12 @@ package com.kfhstu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 消费者模块
  */
+@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ConsumerApplication {
     public static void main(String[] args) {
--- member-service-provider-10000/src/main/java/com/kfhstu/MemberApplication.java
+++ member-service-provider-10000/src/main/java/com/kfhstu/MemberApplication.java
@@ -2,12 +2,14 @@ package com.kfhstu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * this is members application start class
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
